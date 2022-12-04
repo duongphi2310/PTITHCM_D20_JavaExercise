fe596610b44a30ee2646cb2c7f54e8aa0cb874b4
duongphi2310 <115022796+duongphi2310@users.noreply.github.com>
2022-12-04 17:08:08 +0700

    Add files via upload

---
+++ Shape/Rectangle.java
@@ -0,0 +1,38 @@
+public class Rectangle extends Shape {
+    protected double width;
+    protected double length;
+    
+    public Rectangle() {
+        
+    }
+    public Rectangle(double width, double length) {
+        this.width  = width;
+        this.length = length;
+    }
+    public Rectangle(double width, double length, String color, boolean filled) {
+        super (color, filled);
+        this.width  = width;
+        this.length = length;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double getLength() {
+        return length;
+    }
+    public void setLength(double length) {
+        this.length = length;
+    }
+    public double getArea() {
+        return this.length * this.width;
+    }
+    public double getPerimeter() {
+        return (this.width + this.length) * 2;
+    }
+    public String toString() {
+        return this.width + " - " +  this.length + " - " + this.color + " - " + this.filled;
+    }
+}
